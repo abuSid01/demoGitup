56db6c20a83c6e2e93bf2f3da2822b53e9983efa
Abu Sid <abusid2001@gmail.com>
2024-11-17 16:34:15 +0530

    Create dummy.js

---
+++ dummy.js
@@ -0,0 +1,10 @@
+function blooddonate(){
+  var age=20;
+  var weight=60;
+  if(age>=18 && weight<=50){
+    console.log("you are eligible to donate blood group")
+  }
+  else{
+    console.log("Not eligible")
+  }
+}
